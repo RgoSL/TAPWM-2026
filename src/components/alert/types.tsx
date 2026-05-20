e6fc577981582c0a552af6483efe983c620651a6
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2026-05-20 20:43:58 -0300

    feat(alert): create component interface

--- src/components/alert/types.tsx
+++ src/components/alert/types.tsx
@@ -1,1 +1,12 @@
 // Classe de Interface do Alert Criado
+
+// Define um Contrato Que os Componentes de Alerta Devem Seguir
+export interface AlertProps { 
+    title: string; // Título do Alerta
+    message: string; // Conteúdo do Alerta
+    visible: boolean; // Váriavel do Estado da Visibilidade do Alerta
+    onClose: () => void; // Função Interna do Alerta Para Encerrá-lo Sem Retorno 
+
+    // Propriedade de Variações do Alerta
+    type?: 'success' | 'error' | 'warning' | 'info'
+}
