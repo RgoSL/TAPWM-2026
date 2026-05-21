0f651c31d395c5937a6b0e9eba79c52594db68db
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2026-05-20 22:07:35 -0300

    feat(alert): add android version

--- src/components/alert/index.android.tsx
+++ src/components/alert/index.android.tsx
@@ -1,1 +1,18 @@
 // Classe Destinada as Especificidades do Alert na Versão Android
+
+import React, { useEffect } from "react";
+import { Alert as RNAlert } from "react-native";
+import { AlertProps } from "./types";
+
+const AlertAndroid: React.FC<AlertProps> = ({ title, message, visible, onClose }) => {
+    useEffect(() => {
+        if(visible) {
+            RNAlert.alert(title, message, [{text: 'OK', onPress: onClose}]);
+        }
+    }, [visible]);
+    
+    return null;
+
+};
+
+export default AlertAndroid;
